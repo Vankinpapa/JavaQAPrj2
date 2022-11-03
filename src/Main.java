b9986f8a57dda4f7e0b30a9739e4abb3481e9ee1
Sergey DR <svd0490@gmail.com>
2022-11-03 23:52:17 +0500

    add files

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 8567; //это цена билета
+        int bonusWay = price/20; //это формула вычисления бонусных миль
+
+        System.out.println("При цене поездки " + price + " рублей, Вы получаете " + bonusWay + " бунусных миль.");
+
+    }
+
+}
